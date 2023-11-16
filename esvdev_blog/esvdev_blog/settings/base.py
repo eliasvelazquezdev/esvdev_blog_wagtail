844e10ca239a19c1db58caa784b59f7d50cfd5e7
Elias Velazquez <evelazquez2604@gmail.com>
2023-11-16 12:17:28 -0300

    chore: Change media url in base settings

--- esvdev_blog/esvdev_blog/settings/base.py
+++ esvdev_blog/esvdev_blog/settings/base.py
@@ -160,7 +160,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+MEDIA_URL = "/blog/media/"
 
 
 # Wagtail settings
