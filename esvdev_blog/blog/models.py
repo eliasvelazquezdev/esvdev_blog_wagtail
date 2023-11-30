f52c49e5ce0066baab34a3ee16734fe072355fb4
Elias Velazquez <evelazquez2604@gmail.com>
2023-11-30 15:54:11 -0300

    feat: Add programming language field to blog page model

--- esvdev_blog/blog/models.py
+++ esvdev_blog/blog/models.py
@@ -62,6 +62,7 @@ class BlogIndexPage(Page):
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = RichTextField(blank=True)
+    programming_language = models.CharField(blank=True, null=True, max_length=100)
     body = StreamField([
         ('heading', blocks.CharBlock(form_classname="title")),
         ('subtitle', blocks.CharBlock(form_classname="subtitle", required=False)),
@@ -69,7 +70,7 @@ class BlogPage(Page):
         ('image', ImageChooserBlock(required=False)),
         ('quote', blocks.BlockQuoteBlock(required=False)),
         ('video', EmbedBlock(required=False)),
-        ('code_snippet', blocks.TextBlock(form_classname="Code Snippet", required=False))
+        ('code_snippet', blocks.TextBlock(form_classname="Code Snippet", required=False, help_text="Snippet with code"))
     ], use_json_field=True)
     authors = ParentalManyToManyField('blog.Author', blank=True)
     tags = ParentalManyToManyField('Tag', blank=True)
@@ -93,6 +94,7 @@ class BlogPage(Page):
     api_fields = [
         APIField('date', serializer=DateField(format='%a %d %b %Y')),
         APIField('intro'),
+        APIField('programming_language'),
         APIField('first_published_at', serializer=DateTimeField(format='%a %d %b %Y')),
         APIField('latest_revision_created_at', serializer=DateTimeField(format='%a %d %b %Y')),
         APIField('body'),
